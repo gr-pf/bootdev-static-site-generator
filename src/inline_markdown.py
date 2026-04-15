2df2980874e28967430cda737618ab217936da1a
grpf <lastd@DESKTOP-M651MNU.localdomain>
2026-04-15 18:51:58 +0200

    Add C3L4

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -25,10 +25,12 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
 
 
 def extract_markdown_images(text):
-    matches = re.findall(r"\!\[(.*?)\]\((.*?)\)", text)
+    pattern = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    matches = re.findall(pattern, text)
     return matches
 
 
 def extract_markdown_links(text):
-    matches = re.findall(r"\[(.*?)\]\((.*?)\)", text)
+    pattern = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    matches = re.findall(pattern, text)
     return matches
